e37eb36446c5d2030b8b476339edeea87d7cfcde
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-01-18 19:27:33 -0300

    Conexão com o BD

---
+++ models/connection.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+let connection;
+const getConnection = async (collectionName) => {
+  connection = connection
+    || (await MongoClient.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }));
+  return connection.db(DB_NAME).collection(collectionName);
+};
+module.exports = getConnection;
