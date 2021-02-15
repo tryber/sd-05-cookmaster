1ea060a38ece889210209cfec9ccbacf651212b9
saturnian99 <saturnian.dsg@gmail.com>
2021-02-15 19:30:08 -0300

    Initial commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { console.log(`Online on ${PORT}`); });
