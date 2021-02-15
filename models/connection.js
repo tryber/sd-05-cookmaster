f2e1cbb8377c4c5a5364a897891d3059e327a389
saturnian99 <saturnian.dsg@gmail.com>
2021-02-15 19:36:31 -0300

    connection to Mongo

---
+++ models/connection.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+let connection = null;
+
+module.exports = async(collectionName) => {
+  const OPTIONS = {
+    useNewParser: true,
+    useUnifiedTopology: true,
+  };
+  connection = connection || await MongoClient.connect(MONGO_DB_URL, OPTIONS);
+  return connection.db(DB_NAME).collection(collectionName);
+};
