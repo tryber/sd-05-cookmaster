995a0d3dab8dc322d04c713639bb60ef8d942170
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-02-17 20:25:13 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const userRouter = require('./controllers/user');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-  response.send();
+	response.send();
 });
+
+app.use('/', userRouter);
+
+app.listen(3000, () => console.log('nat na 3000'));
