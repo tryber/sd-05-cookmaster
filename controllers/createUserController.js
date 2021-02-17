50c41970a17deb003a3eb9ed8d0b656546166f40
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-02-17 17:11:55 -0300

    primeiro requisito (agora vai mesmo)

--- controllers/createUserController.js
+++ controllers/createUserController.js
@@ -3,7 +3,8 @@ const createUserService = require('../services/createUserService');
 const createUserController = async (req, res) => {
   const { name, email, password } = req.body;
   const response = await createUserService(name, email, password);
-  res.status(201).send(response);
+  console.log(response);
+  res.status(201).json({ user: response });
 };
 
 module.exports = createUserController;
