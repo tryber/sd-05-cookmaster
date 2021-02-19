d2984cd7c7706ea48b260adc59972523c1074692
Larissa Palombo <larissampalombo@gmail.com>
2021-02-19 09:35:20 -0300

    com problemas de timeout

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,7 @@
 // mesma do store
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
 let connection;
