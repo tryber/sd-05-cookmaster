0c18c57cec2f05fecb0f4c6dc99d69ce760939f4
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-02-19 15:42:52 -0300

    Add loginService

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-const loginService = require('../services/loginServices');
-const createToken = require('../auth/createJWT');
+const loginServices = require('../services/loginServices');
+const userModel = require('../models/userModel');
+// const createToken = require('../auth/createJWT');
 
 // Endpoint para fazer o login do usuário
 
@@ -11,9 +13,28 @@ router.post(
   '/login',
   async (req, res) => {
     const { email, password } = req.body;
-    const logUser = await loginService.login(email, password);
-    const tokenUser = await createToken(logUser);
-    res.status(201).json({ tokenUser });
+    const logUser = await loginServices.login(email, password);
+    if (!logUser) { return res.status(400).json({ message: 'Login was not possible' }); }
+
+    const secret = 'teste2021';
+
+    const jwtConfig = {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    };
+
+    const user = await userModel.getEmail(email);
+
+    const { password: _, ...userWithoutPassword } = user;
+    const { _id: id } = userWithoutPassword;
+    const payload = {
+      sub: id,
+      userData: userWithoutPassword,
+    };
+
+    const tokenUser = jwt.sign(payload, secret, jwtConfig);
+    // Retorna token ao usuário
+    return res.status(200).json({ tokenUser });
   },
 );
 
--- services/loginServices.js
+++ services/loginServices.js
@@ -1,13 +1,8 @@
 const userModel = require('../models/userModel');
 
 async function login(email, password) {
-  if (password === undefined || password === '' || !password) {
+  if (!password || !email) {
     return { isError: true, message: 'All fields must be filled', status: 401 };
-  }
-
-  const emailRegex = /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
-  if (!emailRegex.test(email)) {
-    return { isError: true, message: 'Incorrect username or password', status: 401 };
   }
 
   const registeredUser = await userModel.getEmail(email);
