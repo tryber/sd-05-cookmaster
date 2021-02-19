cbcd249f2103fe423f40e09319141921271b69a4
saturnian99 <saturnian.dsg@gmail.com>
2021-02-18 21:22:22 -0300

    register - user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const connection = require('./connection');
+
+const register = async ({ name, email, password, role = 'user' }) => {
+  const emailAlreadyExists = await connection('users');
+
+  if (emailAlreadyExists) return null;
+
+  const newUser = await connection('users')
+    .then((users) => users.insertOne({
+      name,
+      email,
+      password,
+      role,
+    }));
+
+  return {
+    _id: newUser.insertedId,
+    name,
+    email,
+    password,
+    role,
+  };
+};
+
+module.exports = {
+  register,
+};
