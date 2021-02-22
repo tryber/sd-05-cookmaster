f9769d7bec27a5a42215e567ca1fe8b08223732c
Larissa Palombo <larissampalombo@gmail.com>
2021-02-22 16:18:50 -0300

    desespero batendo

--- controller/index.js
+++
@@ -1,9 +0,0 @@
-const rota = require('./UserController');
-const login = require('./loginController');
-const recipeRouter = require('./recipeController');
-
-module.exports = {
-  rota,
-  login,
-  recipeRouter,
-};
